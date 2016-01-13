2a13fd0dbb6d4ec0a63d676e63bb9e7c8a324a2c
Tomek Cejner <tomek@smartrecruiters.com>
2016-01-13 10:55:21 +0100

    Initial Commit

---
+++ Harvester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Harvester
+//
+//  Created by Tomek on 13.01.2016.
+//  Copyright © 2016 SmartRecruiters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
